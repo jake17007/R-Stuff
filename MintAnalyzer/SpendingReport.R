26d4ad4fe76bafda98becc1ae9deac6824d2d900
Jake Beauchamp <jake_c_b@comcast.net>
2015-11-16 22:01:45 -0500

    Added monthly comparison functionality

--- MintAnalyzer/SpendingReport.R
+++ MintAnalyzer/SpendingReport.R
@@ -10,6 +10,8 @@ transactions  <- (read.csv (paste("ExportsFromMint/", file.name, sep = "")))
 # Get rid of the original transaction description provided by the bank
 transactions$Original.Description <- NULL
 
+
+
 # ---------------------------------------------------------------------------
 # make.period : data.frame Date Date -> data.frame
 # Takes a the "transactions" data.frame, a start date, and end date
@@ -17,22 +19,20 @@ transactions$Original.Description <- NULL
 # and including those specified
 # EXAMPLE: df <- make.period(df, "column", "0015-08-20", "0015-10-31")
 
-make.period  <- function(data, col, start.date, end.date){  
+make.period  <- function(df, col, start.date, end.date){  
   # Conver "Date" column to class type Date
-  data[,col] <- as.Date(data[,col], "%m/%d/%Y")
+  df[,col] <- as.Date(df[,col], "%m/%d/%Y")
   # Filter relevent dates
-  data <- with(data, data[(Date >= start.date & Date <= end.date),])
+  df <- with(df, df[(Date >= start.date & Date <= end.date),])
   
-  return(data)
+  return(df)
 }
 # ---------------------------------------------------------------------------
 
+
+
 # Make a dataframe for entire semester
 transactions <- make.period(transactions, "Date", "0015-08-20", "0015-10-31")
-
-# Make a dataframe for individual months
-transactions.sept <- make.period(transactions, "Date", "0015-09-01", "0015-09-30")
-transactions.oct <- make.period(transactions, "Date", "0015-10-01", "0015-10-31")
 
 # Make a list of all categories
 list.of.categories <- levels(factor(transactions$Category))
@@ -40,7 +40,7 @@ list.of.categories <- levels(factor(transactions$Category))
 
 
 # ---------------------------------------------------------------------------
-# get.category.totals : -> data.frame
+# get.category.totals : data.frame -> data.frame
 # Produces a vector containing the sums of the amounts spent in the 
 # respective categories
 
@@ -49,11 +49,12 @@ get.category.totals <- function(df) {
   cat.totals <- NULL
   
   for (i in list.of.categories) {
-    cat.totals <- c(category.totals, 
-                         sum(df$Amount[which(df$Category == i & 
-                                             df$Transaction.Type == "debit")])) 
-    return(cat.totals)
+    cat.totals <- c(cat.totals, 
+                         sum(df[,"Amount"][which(df[,"Category"] == i & 
+                                             df[,"Transaction.Type"] == "debit")])) 
   }
+  
+  return(cat.totals)
 }
 # ---------------------------------------------------------------------------
 
@@ -70,71 +71,71 @@ spending.by.cat <- spending.by.cat[order(-category.totals),]
 counts <- table(spending.by.cat$category.totals)
 
 par(las=2)
-par(mar=c(10,1,1,1))
+par(mar=c(10,5,1,1))
+barplot(spending.by.cat$category.totals,
+        main="Fall 2015 Semester Spending By Category",
+        xlab="Category",
+        ylab="Spending",
+        names.arg=spending.by.cat$list.of.categories)
+
+
+#######
+
+# Remove unneeded categories of spending
+spending.by.cat.min <- spending.by.cat[3:nrow(spending.by.cat),]
+
+# Plot again
+par(las=2)
+par(mar=c(10,5,1,1))
 barplot(spending.by.cat.min$category.totals,
         main="Fall 2015 Semester Spending By Category",
         xlab="Category",
         ylab="Spending",
         names.arg=spending.by.cat.min$list.of.categories)
 
-spending.by.cat.min <- spending.by.cat[3:nrow(spending.by.cat),]
 
 
 
+
+
+
+### *********************************************************************************
 
 ### Make a bar plot of spending by different months "Spending by Months"
 
+# Make a transactions dataframe for individual months
+transactions.sept <- make.period(transactions, "Date", "0015-09-01", "0015-09-30")
+transactions.oct <- make.period(transactions, "Date", "0015-10-01", "0015-10-31")
+
+# Get category spending totals for individual months
+# - September
 category.totals.sept <- get.category.totals(transactions.sept)
+# - October
+category.totals.oct <- get.category.totals(transactions.oct)
 
-spending.by.cat.sept <- data.frame(list.of.categories, category.totals.sept)
+# Make a dataframe combining months
+combined.months <- data.frame(September = category.totals.sept, 
+                              October = category.totals.oct,
+                              row.names = list.of.categories)
 
+# Make a column to be used for labels
+combined.months$Category <- row.names(combined.months)
 
+# Bring data to long format needed for ggplot
+require(reshape2)
+combined.months.long <- melt(combined.months, value.name="Spending", variable.name="Month", na.rm=TRUE)
 
-
-
-
-
-
-
-
-
-
-
-par(mar=c(1,1,1,1))
-
-amounts
-
-cat.table <- table(spending.by.cat)
-cat.table
+# plot and facet by categories
+require(ggplot2)
+qplot( data=combined.months.long, x = Month, y = Spending, geom="bar", stat = "identity" ) + facet_wrap( "Category" )
 
 
 
+######
 
+# Remove unneeded categories of spending
+to.be.removed <- which(combined.months.long$Category == "Mortgage & Rent")
+combined.months.long.min <- combined.months.long[-to.be.removed,]
+# Plot again
+qplot( data=combined.months.long.min, x = Month, y = Spending, geom="bar", stat = "identity" ) + facet_wrap( "Category" )
 
-
-
-sum(transactions$Amount[which(transactions$Category == "Credit Card Payment" & transactions$Transaction.Type == "debit")])
-sum(transactions$Amount[which(transactions$Category == "Casino Gambling" & transactions$Transaction.Type == "debit")])
-
-category.totals <- NULL
-
-for (i in list.of.categories) {
-  category.totals <- c(category.totals, sum(transactions$Amount[which(transactions$Category == i & 
-                                                                        transactions$Transaction.Type == "debit")])) 
-}
-
-tester.totals <- NULL
-
-for (i in list.of.categories) {
-  tester.totals <- c(tester.totals, i)
-}
-
-
-
-
-
-cat.table <- table(transactions$Category, transactions$Amount)
-cat.table
-
-
-with(transactions, transactions[])
